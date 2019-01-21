7d00b2dda02783d9876326d171983c963646d760
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-01-21 15:05:56 +0300

    [Start #163359789]Fix landing page.

--- run.py
+++ run.py
@@ -6,14 +6,23 @@ def choose_option():
     """Selecting an action to perform."""
     print("1. Register an account.")
     print("2. Login to your account.")
-    option = input("Please choose an action: ")
-    if isinstance(option, str):
-        print("Please enter a valid action!")
+    option = int(input("Please choose an action: "))
+    if option == 1:
+        # firstname, lastname, username, password, password2,
+        firstname = str(input("Enter first name:"))
+        lastname = str(input("Enter last name:"))
+        username = str(input("Enter username:"))
+        password = str(input("Enter password:"))
+        password2 = str(input("Confirm password:"))
+        user = UserModels(firstname, lastname, username, password, password2)
+        register_user = user.signup()
+        return register_user
+    elif option == 2:
+        username = str(input("Enter username:"))
+        password = str(input("Enter password:"))
+        # user = UserModels()
+        login_user = UserModels.login(username, password)
+        return login_user
     else:
-        if option == 1:
-            UserModels.signup()
-        elif option == 2:
-            UserModels.login()
-        else:
-            print("Not a valid option. Please enter a valid option.")
+        print("Not a valid option. Please enter a valid option.")
 choose_option()
