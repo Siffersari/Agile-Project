166952d321ce49019499517e8fc6f5e88cd2bd5b
LeewelKarani <Leewelkarani@gmail.com>
2019-01-21 12:14:25 +0300

    Add basic template

---
+++ user_models.py
@@ -0,0 +1,25 @@
+users = []
+
+
+class UserModels(object):
+    """
+    This class UserModels contains
+    methods for user instances
+    """
+
+    def __init__(self, details={}):
+
+        self.user_details = details
+        self.db = users
+
+    def signup(self):
+        pass
+
+    def login(self):
+        pass
+
+    def create_comment(self):
+        pass
+
+    def edit_comment(self):
+        pass
